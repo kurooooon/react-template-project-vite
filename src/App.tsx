d6dfe782b24fbcf9e26f09efebf775e6277cbf89
kurooooon <b1006110@gmail.com>
2021-11-20 14:47:26 +0900

    fix app

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import dogImage from "./images/dog.jpg";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello React!</h1>
-        <img src={dogImage} />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <img src={dogImage} />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector("#app"));
